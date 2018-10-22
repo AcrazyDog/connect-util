6d908a20e5d88697fb67bf64923454fc5f8a311f
sy <lsychymzyl@163.com>
2018-10-22 14:04:44 +0800

    delete file head kingdee

--- src/main/java/com/sy/connect/util/mongo/repository/MongoRepository.java
+++ src/main/java/com/sy/connect/util/mongo/repository/MongoRepository.java
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2006-2015 Kingdee Ltd. All Rights Reserved. 
- *  
- * This code is the confidential and proprietary information of   
- * Kingdee. You shall not disclose such Confidential Information   
- * and shall use it only in accordance with the terms of the agreements   
- * you entered into with Kingdee,http://www.Kingdee.com.
- *  
- */
 package com.sy.connect.util.mongo.repository;
 
 import java.util.List;
